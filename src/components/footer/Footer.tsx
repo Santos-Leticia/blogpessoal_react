d0e710c9ff28686fc4fc4016f6680599175dff5f
Santos-Leticia <le.ticiasoliveira@outlook.com>
2025-08-30 17:45:33 -0300

    Implementa rotas

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -14,7 +14,9 @@ function Footer() {
                         </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
+                        <a href="www.linkedin.com/in/devtissa" target="_blank">
                         <LinkedinLogoIcon size={48} weight='bold' />
+                        </a>
                         <InstagramLogoIcon size={48} weight='bold' />
                         <FacebookLogoIcon size={48} weight='bold' />
                     </div>
