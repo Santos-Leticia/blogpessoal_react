48e58fa8bf9825100278605863f904695ebabb32
Santos-Leticia <le.ticiasoliveira@outlook.com>
2025-08-30 18:13:43 -0300

    Adiciona a pagina login

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 import Cadastro from './pages/cadastro/Cadastro'
+import Login from './pages/login/Login'
 
 function App() {
 
@@ -14,7 +15,8 @@ function App() {
     <div className="min-h-[80vh]">
       <Routes>
         <Route path='/home' element={<Home />} />
-        <Route path='/' element={<Cadastro />} />
+        <Route path='/cadastro' element={<Cadastro />} />
+        <Route path='/' element={<Login />} />
       </Routes>
     </div>
     <Footer/>
